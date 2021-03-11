88ecd9d64b80cab96e9db91595ffc9bca6ac67a4
jzlly <987857521@qq.com>
2021-03-11 23:24:01 +0800

    整数反转

---
+++ src/com/jz/datastructure/Reverse.java
@@ -0,0 +1,24 @@
+package com.jz.datastructure;
+
+import org.junit.Test;
+
+public class Reverse {
+
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+    @Test
+    public void testReverse(){
+        int reverse = reverse(123456);
+        System.out.println(reverse);
+    }
+
+}
